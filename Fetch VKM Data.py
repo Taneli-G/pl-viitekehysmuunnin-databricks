a8186749a6a0288bc9871f79d1f19c3042d186e2
Taneli-G <Taneli-G>
2024-04-12 14:35:03 +0000

    Write fetched data to a table

--- Fetch VKM Data.py
+++ Fetch VKM Data.py
@@ -1,17 +1,41 @@
 # Databricks notebook source
+# MAGIC %pip install geopandas geojson
+
+# COMMAND ----------
+
 import requests
 from pyspark.sql import SparkSession
 import json
 
 spark = SparkSession.builder.getOrCreate()
 
-url = "https://avoinapi.vaylapilvi.fi/vaylatiedot/ogc/features/v1/collections/tiestotiedot:tieosoiteverkko/items?f=application%2Fgeo%2Bjson&&filter=tie=1"
-file_name = '/Volumes/pl_viitekehysmuunnin/vkm_data/vkm_data_json/response.json'
+# COMMAND ----------
+
+# Specify target URL and Databricks filename and Volume
+url = "https://avoinapi.vaylapilvi.fi/vaylatiedot/ogc/features/v1/collections/tiestotiedot:tieosoiteverkko/items?f=application%2Fgeo%2Bjson"
+volume = "/Volumes/pl_viitekehysmuunnin/vkm_data/vkm_data_json/"
+file_name = "response.json"
+
+# Make the request
 response_json = requests.get(url).json()
 
-with open(file_name, 'w') as json_file:
+# Write the request response to a Databricks volume
+with open(volume + file_name, 'w') as json_file:
     json.dump(response_json, json_file)
 
-df = spark.read.format("json").json(file_name)
-df.show()
+# COMMAND ----------
 
+# Read the file from the volume in to a spark dataframe
+df = spark.read.json(f"dbfs:{volume}{file_name}")
+
+spark.sql("CREATE TABLE IF NOT EXISTS pl_viitekehysmuunnin.vkm_data.tieosoiteverkko_bronze")
+(
+df.write
+    .option("mergeSchema", True)
+    .insertInto("pl_viitekehysmuunnin.vkm_data.tieosoiteverkko_bronze", overwrite=True)
+)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM pl_viitekehysmuunnin.vkm_data.tieosoiteverkko_bronze
